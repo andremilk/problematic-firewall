17bfe2bdacab60f86b9ae99bd0b8f71462820c6f
Andre Leite <andre.guimaraes.leite@gmail.com>
2016-03-10 11:22:47 +0000

    Adds function to restart an interface and a function to help generate timestamped errors.

--- dumb-solution.sh
+++ dumb-solution.sh
@@ -14,3 +14,29 @@ try_to_connect() {
     ssh -i $key_path -p $port $username@$server_host exit;
     return $?
 }
+
+# $1 should be the interface
+restart_interface() {
+    iface=$1;
+    debug_log ifdown $iface;
+    debug_log ifup $iface;
+}
+
+# This will output errors to a file called errors
+# $1 the command
+debug_log() {
+    $1 2> tmp; # runs the command and gets the errors to stdout
+    while read line ; do # reads stdout
+        echo "$(date): $(caller 0)" >> errors; # echoes the output to a file
+    done < tmp
+
+    if [[ $(wc -l <tmp) -ge 1 ]]
+    then
+        echo "There's been an error getting the interface down";
+        echo "Check the error logfile";
+        exit;
+    else
+        rm tmp;
+        return 0;
+    fi
+}
