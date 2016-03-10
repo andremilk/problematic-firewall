5223281b417930348b39104f1031ca357c8c5e56
Andre Leite <andre.guimaraes.leite@gmail.com>
2016-03-10 11:57:51 +0000

    Adds a cooldown checking function

--- dumb-solution.sh
+++ dumb-solution.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
 
+# this function should check if the cooldown has finished
+# $1 should be the file
+# $2 should be the cooldown in minutes
+check_cooldown() {
+    if [ -a $1 ]
+    then
+        echo "arquivo existe";
+        time_elapsed=$(expr $(date +%s) - $(date +%s -r abc));
+        echo $time_elapsed;
+        if [ $time_elapsed -gt $(($2*60)) ] # if the file os older than $2 * 60 seconds 
+        then
+            rm $1; # removes the file and the cooldown.
+            return 0;
+        else
+            return -1;
+        fi
+    fi
+    return 0;
+}
+
 # $1 should be the host
 # $2 username
 # $3 the key path
