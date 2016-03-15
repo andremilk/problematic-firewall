2f3be9c46e0f248d844c56742ed77e5d1577995a
André Leite <andre.guimaraes.leite@gmail.com>
2016-03-15 15:59:14 -0300

    Implements the main logic of the script

--- dumb-solution.sh
+++ dumb-solution.sh
@@ -1,5 +1,58 @@
 #!/bin/bash
+# the server tries to connect to another machine through ssh
+# if it fails, it then restarts the interface and tries one more time..
+# if it fails on a second attempt, then it will wait for some time before going back to the begining
 
+# perhaps these should be set on environment variables
+# $1 should be the sleeping time in minutes
+# $2 host
+# $3 username
+# $4 key path
+# $5 port
+# $6 interface
+main() {
+    SLEEPING_TIME=$1;
+    try_to_connect "$2" "$3" "$4" "$5"; # first attempt
+    if [[ $? -ne 0 ]]
+    then
+        # did not connect on the first attempt
+        echo "$(date): (ssh) #first-attempt failed" >> log;
+        check_cooldown /tmp/script-lock $(($SLEEPING_TIME*60));
+        time_left=$?;
+        if [[ $time_left -eq 0 ]] # restarts the interface and retry
+        then
+            echo "OPA aqui foi $6";
+            restart_interface "$6";
+            echo "foi?";
+            sleep 5;
+            try_to_connect "$2" "$3" "$4" "$5";
+            if [[ $? -ne 0 ]] # if it failed again, the lock should be activated.
+            then
+                echo "$(date): (ssh) #second-attempt failed" >> log;
+                lock; #lock
+                exit 0;
+            else # it connected on the second attempt, the lock should be released.
+                echo "$(date): (ssh) #second-attempt success" >> log;
+                unlock;
+                exit 0;
+
+            fi
+        fi
+    else
+        # ok, it connected on the first attempt remove any possible locks
+        echo "$(date): (ssh) #first-attempt success" >> log;
+        unlock;
+        exit 0;
+    fi
+}
+
+lock() {
+    touch /tmp/script-lock;
+}
+
+unlock() {
+    rm -f /tmp/script-lock;
+}
 
 # this function should check if the cooldown has finished
 # $1 should be the file
@@ -7,15 +60,16 @@
 check_cooldown() {
     if [ -a $1 ]
     then
-        echo "arquivo existe";
-        time_elapsed=$(expr $(date +%s) - $(date +%s -r abc));
-        echo $time_elapsed;
-        if [ $time_elapsed -gt $(($2*60)) ] # if the file os older than $2 * 60 seconds 
+        time_elapsed=$(expr $(date +%s) - $(date +%s -r $1));
+        time_left=$(expr $(($2*60)) - $time_elapsed);
+        if [ $time_elapsed -gt $(($2*60)) ] # if the file os older than $2 * 60 seconds
         then
             rm $1; # removes the file and the cooldown.
+            echo "$(date): lock removed" >> log;
             return 0;
         else
-            return -1;
+            echo "$(date): lock still active" >> log;
+            exit 0; # the lock is active, the script should exit.
         fi
     fi
     return 0;
@@ -26,37 +80,18 @@ check_cooldown() {
 # $3 the key path
 # $4 port
 try_to_connect() {
-    server_host=$1;
-    username=$2
-    key_path=$3;
-    port=$4;
-
-    ssh -i $key_path -p $port $username@$server_host exit;
-    return $?
+    server_host="$1";
+    username="$2";
+    key_path="$3";
+    port="$4";
+    ssh -i "$key_path" -p "$port" $username@$server_host "exit" 2> errors;
+    return $result
 }
 
 # $1 should be the interface
 restart_interface() {
     iface=$1;
-    debug_log ifdown $iface;
-    debug_log ifup $iface;
-}
-
-# This will output errors to a file called errors
-# $1 the command
-debug_log() {
-    $1 2> tmp; # runs the command and gets the errors to stdout
-    while read line ; do # reads stdout
-        echo "$(date): $(caller 0)" >> errors; # echoes the output to a file
-    done < tmp
-
-    if [[ $(wc -l <tmp) -ge 1 ]]
-    then
-        echo "There's been an error getting the interface down";
-        echo "Check the error logfile";
-        exit;
-    else
-        rm tmp;
-        return 0;
-    fi
+    ifdown "$iface";
+    ifup "$iface";
+    echo "$(date): restarted interface" >> log;
 }
